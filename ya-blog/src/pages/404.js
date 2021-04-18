f76b52d2d0984b0eb21276e1620d16ab8c4a77a9
Yassui <yassui.kimura@gmail.com>
2021-04-18 18:26:56 -0300

    feat: cleanup starter default config

---
+++ ya-blog/src/pages/404.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>404: Not Found</h1>
+    <p>Sorry, It's so lonely here...</p>
+  </Layout>
+)
+
+export default NotFoundPage
